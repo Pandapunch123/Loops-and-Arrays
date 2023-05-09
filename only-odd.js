e66fea0b40989af0fa6adb37b08971b96fe041e9
Torra Tran <pandapunch@Torras-MacBook-Air.local>
2023-05-08 17:12:56 -0700

    done

--- only-odd.js
+++ only-odd.js
@@ -6,8 +6,8 @@ console.log([numbers[4], numbers[6]]);
 const odds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(odds[0], odds[2], odds[4], odds[6], odds[8]);
 
-const odd = (70, 42, 55, 81, 21, 91, 34);
-console.log(odd[0], odd[2], odd[4], odd[6], odd[8]);
+const odd = [70, 42, 55, 81, 21, 91, 34];
+console.log(odd[0], odd[2], odd[4], odd[6]);
 
 
 const number = new Array(2, 4, 6, 8, 10, 11, 12);
